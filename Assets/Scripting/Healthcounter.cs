480dd0dac922f9ed4b30b1090c9ecf1912f70597
surgeofpower <https://github.com/Deep-Analysis/surgeofpower.git>
2018-11-05 14:50:26 +0000

    initial save
    all files

---
+++ Assets/Scripting/Healthcounter.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthcounter : MonoBehaviour
+{
+    public UnityEngine.UI.Text HealthDisplay;
+    public UnityEngine.UI.Text HealthDisplay2;
+    public UnityEngine.UI.Text HealthDisplay3;
+    public UnityEngine.UI.Text HealthDisplay4;
+    public int Health = 0;
+    public int Healthperclick = 1;
+    public int HealthperClick = -1;
+
+    //player1
+    public void clicked()
+    {
+        Health += Healthperclick;
+        HealthDisplay.text = "Health:  " + Health;
+    }
+
+    public void Clicked()
+    {
+        Health += HealthperClick;
+        HealthDisplay.text = "Health:  " + Health;
+    }
+    //player2
+    public void clicked2()
+    {
+        Health += Healthperclick;
+        HealthDisplay2.text = "Health:  " + Health;
+    }
+
+    public void Clicked2()
+    {
+        Health += HealthperClick;
+        HealthDisplay2.text = "Health:  " + Health;
+    }
+
+    //player3
+    public void clicked3()
+    {
+        Health += Healthperclick;
+        HealthDisplay3.text = "Health:  " + Health;
+    }
+
+    public void Clicked3()
+    {
+        Health += HealthperClick;
+        HealthDisplay3.text = "Health:  " + Health;
+    }
+
+        //player4
+        public void clicked4()
+        {
+            Health += Healthperclick;
+            HealthDisplay4.text = "Health:  " + Health;
+        }
+
+        public void Clicked4()
+        {
+            Health += HealthperClick;
+            HealthDisplay4.text = "Health:  " + Health;
+        }
+    }
---
+++ Assets/Scripting/ManaCount.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaCount : MonoBehaviour
+{
+    public UnityEngine.UI.Text ManaDisplay;
+    public UnityEngine.UI.Text ManaDisplay2;
+    public UnityEngine.UI.Text ManaDisplay3;
+    public UnityEngine.UI.Text ManaDisplay4;
+    public int Mana = 0;
+    public int Manaperclick = 1;
+    public int ManaperClick = -1;
+
+    //player1
+    public void clicked()
+    {
+        Mana += Manaperclick;
+        ManaDisplay.text = "Mana:  " + Mana;
+    }
+
+    public void Clicked()
+    {
+        Mana += ManaperClick;
+        ManaDisplay.text = "Mana:  " + Mana;
+    }
+    //player2
+    public void clicked2()
+    {
+        Mana += Manaperclick;
+        ManaDisplay2.text = "Mana:  " + Mana;
+    }
+
+    public void Clicked2()
+    {
+        Mana += ManaperClick;
+        ManaDisplay2.text = "Mana:  " + Mana;
+    }
+    //player3
+    public void clicked3()
+    {
+        Mana += Manaperclick;
+        ManaDisplay3.text = "Mana:  " + Mana;
+    }
+
+    public void Clicked3()
+    {
+        Mana += ManaperClick;
+        ManaDisplay3.text = "Mana:  " + Mana;
+    }
+    //player4
+    public void clicked4()
+    {
+        Mana += Manaperclick;
+        ManaDisplay4.text = "Mana:  " + Mana;
+    }
+
+    public void Clicked4()
+    {
+        Mana += ManaperClick;
+        ManaDisplay4.text = "Mana:  " + Mana;
+    }
+}
